23a6b210dd1a453bd6160a04426afe0f81d4cd15
Hatef tavakkoli <tavakkoli.ht@gmail.com>
2019-11-28 16:18:44 +0330

    hatef

---
+++ src/test/java/coach/barnamenevis/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package coach.barnamenevis.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
