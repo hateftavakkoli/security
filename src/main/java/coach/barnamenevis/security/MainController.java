295177f8632b6ccbcd11e40f513092e50a32cdf3
Hatef tavakkoli <tavakkoli.ht@gmail.com>
2019-11-29 20:24:42 +0330

    hatef

--- src/main/java/coach/barnamenevis/security/MainController.java
+++ src/main/java/coach/barnamenevis/security/MainController.java
@@ -1,9 +1,16 @@
 package coach.barnamenevis.security;
 
+import coach.barnamenevis.security.users.domain.Users;
 import coach.barnamenevis.security.users.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 @Controller
 public class MainController {
@@ -26,9 +33,35 @@ public class MainController {
     }
 
     @GetMapping("/admin")
-    public String adminPage() {
+    public String adminPage(Model model) {
+        model.addAttribute("users",usersService.findAll());
         return "admin";
     }
+
+    @GetMapping(value = "/admin/register")
+    public String registerPage(Model model) {
+        model.addAttribute("user", new Users());
+        return "registerUser";
+    }
+
+    @GetMapping(value = "/admin/edit/{id}")
+    public String registerPage(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", usersService.findById(id));
+        return "registerUser";
+    }
+
+    @GetMapping(value = "/admin/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        usersService.deleteById(id);
+        return "redirect:/admin";
+    }
+
+    @PostMapping(value = "/admin/register")
+    public String register(@ModelAttribute(name = "user") Users users) {
+        usersService.registerUser(users);
+        return "redirect:/admin";
+    }
+
 
     @GetMapping("/login")
     public String loginPage() {
--- src/main/java/coach/barnamenevis/security/users/service/UsersService.java
+++ src/main/java/coach/barnamenevis/security/users/service/UsersService.java
@@ -1,11 +1,14 @@
 package coach.barnamenevis.security.users.service;
 
+import coach.barnamenevis.security.users.domain.Users;
 import coach.barnamenevis.security.users.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsersService implements UserDetailsService {
@@ -21,5 +24,21 @@ public class UsersService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return usersRepository.findByEmail(email);
+    }
+
+    public List<Users> findAll() {
+        return usersRepository.findAll();
+    }
+
+    public Users findById(Long id) {
+        return usersRepository.getOne(id);
+    }
+
+    public void deleteById(Long id) {
+        usersRepository.deleteById(id);
+    }
+
+    public void registerUser(Users users) {
+        usersRepository.save(users);
     }
 }
