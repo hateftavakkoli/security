3e58deace10c1ad89fa4aa8f1d516c40c2e446ac
Hatef tavakkoli <tavakkoli.ht@gmail.com>
2019-12-23 14:03:09 +0330

    hatef

--- src/main/java/coach/barnamenevis/security/MainController.java
+++ src/main/java/coach/barnamenevis/security/MainController.java
@@ -4,6 +4,7 @@ import coach.barnamenevis.security.users.domain.Users;
 import coach.barnamenevis.security.users.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -68,6 +69,11 @@ public class MainController {
     @GetMapping("/login")
     public String loginPage() {
         return "login";
+    }
+
+    @GetMapping("/error")
+    public String errorPage() {
+        return "error";
     }
 
 }
