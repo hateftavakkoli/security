3b5c63d4622c5f4b713dcee3511b2446a82b00f2
Hatef tavakkoli <tavakkoli.ht@gmail.com>
2020-04-30 14:19:59 +0430

    hatef

--- src/main/java/coach/barnamenevis/security/config/LoginSuccessHandler.java
+++ src/main/java/coach/barnamenevis/security/config/LoginSuccessHandler.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,11 +17,12 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+
         if (authentication.getAuthorities().contains(Authority.OP_ACCESS_ADMIN)) {
-            redirect.sendRedirect(httpServletRequest,httpServletResponse,"/admin");
-        } else if(authentication.getAuthorities().contains(Authority.OP_ACCESS_USER)) {
-            redirect.sendRedirect(httpServletRequest,httpServletResponse,"/user");
+            redirect.sendRedirect(httpServletRequest, httpServletResponse, "/admin");
+        } else if (authentication.getAuthorities().contains(Authority.OP_ACCESS_USER)) {
+            redirect.sendRedirect(httpServletRequest, httpServletResponse, "/user");
         } else
-            redirect.sendRedirect(httpServletRequest,httpServletResponse,"/");
+            redirect.sendRedirect(httpServletRequest, httpServletResponse, "/");
     }
 }
